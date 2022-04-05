240e9fed5b1b747f1bf25044f37a407776c209db
David Gardiner <dgardi@gmail.com>
2022-04-04 22:28:05 -0400

    wireframed, added some CSS, and started brainstorming js

---
+++ script.js
@@ -0,0 +1,15 @@
+//Variables
+
+const APIKey="9f4c90cacbdde296f87c472d5696eb06"
+
+// Call Example: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// Call API on click to Get Weather Data for Searched City. City is added to search history. 
+
+// Parse  response to page. Should include city name, date, weather icon, temperature, humidity and UV Index.
+
+// Change UVIndex Green/Yellow/Red
+
+// Get 5 day future data. Should include 5 day forecast displaying date, weather icon, temp, wind speed, and humidity.
+
+// When city in search history is clicked, re-run script with that city. 
